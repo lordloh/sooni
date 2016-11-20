834e6c468fade9c26609ab7122ffb923a432aed5
Lord Bharath Bhushan Lohray <bharath@lohray.com>
2016-11-20 16:25:52 -0600

    A scirpt to read and display submitted emails.

---
+++ list_emails.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+import sqlite3
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+sqlite_db="sooni.db"
+email_db=sqlite3.connect(sqlite_db)
+email_db.row_factory = dict_factory
+c = email_db.cursor()
+c.execute("SELECT * From email")
+email_list = c.fetchall()
+for e in email_list:
+    print(e['email'].ljust(50)+e['date'])
+print("Total :"+str(len(email_list)))

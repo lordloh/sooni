4c5cbb9c7e038d42a1b47f485aa0cf5c9b307a0d
Lord Bharath Bhushan Lohray <bharath@lohray.com>
2016-12-01 03:36:52 -0600

    Save IP & User Agent

--- save_survey.php
+++ save_survey.php
@@ -5,17 +5,30 @@ $table = 'survey';
 $json_output="{";
 $all_keys=array_keys($_POST);
 $all_data_fields=['num_domains','shark','host'];
-$sqlite_type=['INTEGER','INTEGER','STRING'];
+$all_data_fields_type=['INTEGER','INTEGER','STRING'];
+$other_data=['REMOTE_ADDR','HTTP_USER_AGENT'];
+$other_data_type=['STRING','STRING'];
+$all_fields=array_merge($all_data_fields,$other_data);
+$sqlite_type=array_merge($all_data_fields_type,$other_data_type);
 $sqlite_list="";
 $sqlite_field="";
 $sqlite_values="";
 foreach($all_data_fields as $k=>$dt){
-    $sqlite_list.=$dt." ".$sqlite_type[$k].", ";
+    $sqlite_list.=$dt." ".$all_data_fields_type[$k].", ";
     $sqlite_field.=$dt.", ";
-    if($sqlite_type[$k]=="STRING"){
-        $sqlite_values.='"'.$_POST[$dt].'", ';
+    if($all_data_fields_type[$k]=="STRING"){
+        $sqlite_values.='"'.fix_inputs($_POST[$dt]).'", ';
     }else{
         $sqlite_values.=$_POST[$dt].', ';
+    }
+}
+foreach($other_data as $k=>$dt){
+    $sqlite_list.=$dt." ".$other_data_type[$k].", ";
+    $sqlite_field.=$dt.", ";
+    if($other_data_type[$k]=="STRING"){
+        $sqlite_values.='"'.$_SERVER[$dt].'", ';
+    }else{
+        $sqlite_values.=$_SERVER[$dt].', ';
     }
 }
 $sqlite_list=trim($sqlite_list,", ");
@@ -23,20 +36,25 @@ $sqlite_field=trim($sqlite_field,", ");
 $sqlite_values=trim($sqlite_values,", ");
 
 $all_present=true;
-foreach($all_data_fields as $field){
-    $pr=in_array($field,$all_keys);
-    $all_present=$all_present && $pr;
-}
-
-if ($all_present){
+$missing_fields = array_diff($all_data_fields, $all_keys);
+if (count($missing_fields)==0){
     $email_db = new SQLite3($DB,SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
     $email_db->busyTimeout(1500);
     $email_db->exec('CREATE TABLE IF NOT EXISTS '.$table.' ('.$sqlite_list.');');
     $ins = $email_db->exec("INSERT INTO $table (".$sqlite_field.") VALUES(".$sqlite_values.")");
     $json_output.='"result":0, "msg":"okay"';
 }else{
-    $json_output.='"result":1, "msg":"missing data parts"';
+    $json_output.='"result":1, "msg":"missing data parts","missing":["'.implode('","',$missing_fields).'"]';
 }
 $json_output.="}";
 echo $json_output;
+
+function fix_inputs($val){
+    if (is_array($val)){
+        $o=implode(",",$val);
+        return $o;
+    }else{
+     return $val;   
+    }
+}
 ?>
